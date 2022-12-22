886205fdd7875229e22d8a19230d49f38733bef5
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-22 10:19:39 +0900

    Update top.py

--- top.py
+++ top.py
@@ -2,3 +2,20 @@ import streamlit as st
 
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
+
+
+import numpy as np
+import cv2
+from insightface.app import FaceAnalysis
+ 
+image_file = "input.jpg"
+img = cv2.imread(image_file)
+ 
+app = FaceAnalysis()
+app.prepare(ctx_id=0, det_size=(640, 640))
+ 
+faces = app.get(np.asarray(img))
+print("faces:" + str(len(faces)))
+ 
+rimg = app.draw_on(img, faces)
+st.image(ring)
