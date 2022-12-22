e63fb08143d586386206b8adbd7de01937f76e49
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-22 10:35:45 +0900

    Update top.py

--- top.py
+++ top.py
@@ -11,7 +11,7 @@ from insightface.app import FaceAnalysis
 image_file = "input.jpg"
 img = cv2.imread(image_file)
  
-app = FaceAnalysis()
+app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(640, 640))
  
 faces = app.get(img)
