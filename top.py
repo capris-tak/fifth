a50e44b34efe9206bf4b499425bce7c4ed7233dc
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-22 11:13:10 +0900

    Update top.py

--- top.py
+++ top.py
@@ -7,21 +7,22 @@ import cv2
 
 uploaded_image = st.file_uploader('Choose an image..',type=['png', 'jpg','jpeg','webp'])
 
-XML_PATH = "haarcascade_frontalface_default.xml"
-#INPUT_IMG_PATH = "input.jpg"
-#OUTPUT_IMG_PATH = "出力する画像のパス"
- 
-classifier = cv2.CascadeClassifier(XML_PATH)
- 
-img = cv2.imread(uploaded_image)
-color = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
- 
-targets = classifier.detectMultiScale(color)
- 
-for x, y, w, h in targets:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
- #cv2.imwrite(OUTPUT_IMG_PATH, img)
-st.image(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+if uploaded_image is not None:
+ XML_PATH = "haarcascade_frontalface_default.xml"
+ #INPUT_IMG_PATH = "input.jpg"
+ #OUTPUT_IMG_PATH = "出力する画像のパス"
+
+ classifier = cv2.CascadeClassifier(XML_PATH)
+
+ img = cv2.imread(uploaded_image)
+ color = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ targets = classifier.detectMultiScale(color)
+
+ for x, y, w, h in targets:
+     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+  #cv2.imwrite(OUTPUT_IMG_PATH, img)
+ st.image(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
  
 
 
