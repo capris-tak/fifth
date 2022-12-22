80587fd0789346f2b58dab7c5dbd110e653cb908
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-22 10:45:58 +0900

    Update top.py

--- top.py
+++ top.py
@@ -14,11 +14,9 @@ img = cv2.imread(image_file)
 app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(640, 640))
  
-faces = app.get(img)
+faces = app.get(np.asarray(img))
 rimg = app.draw_on(img, faces)
 
-#faces = app.get(np.asarray(img))
-st.write("faces:" + str(len(faces)))
- 
-rimg = app.draw_on(img, faces)
+#st.write("faces:" + str(len(faces)))
+#rimg = app.draw_on(img, faces)
 st.image(rimg)
